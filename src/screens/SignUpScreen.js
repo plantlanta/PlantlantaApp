8967512fda41199e30e513221a7ee2a35c89c679
16saadc <16saadc@gmail.com>
2019-08-30 11:47:30 -0400

    first registration screen commit

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,23 +1,67 @@
-import React, { Component } from 'react'
+import * as WebBrowser from 'expo-web-browser';
+import React, { Component } from 'react';
+import CheckBox from 'react-native-check-box'
 import {
-    StyleSheet,
-    View,
-    Text,
-} from 'react-native'
-export default class SignUpScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Say something</Text>
-            </View>
-        )
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+export default class RegistrationScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false,
     }
+  }
+
+
+  render () {
+    return (
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 27 }}>Create An Account</Text>
+        <TextInput placeholder="First Name" />
+        <TextInput placeholder="Last Name" />
+        <TextInput placeholder="Username" />
+        <TextInput placeholder="Password" />
+        <TextInput placeholder="Confirm Password" />
+        <View style={{ margin: 7 }} />
+        <Button onPress={null} title="Register" />
+        <CheckBox
+            style={{flex: 1, padding: 10}}
+            onClick={()=>{
+              this.setState({
+                  isChecked:!this.state.isChecked
+              })
+            }}
+            isChecked={this.state.isChecked}
+            rightText={"test"}
+        />
+        <Text
+          style={{ color: "blue" }}
+          onPress={this._navigateToRegister}
+          Register
+        />
+      </View>
+    );
+  }
 }
+
+// RegistrationScreen.navigationOptions = {
+//   title: 'Registration',
+// };
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#aa73b7',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
