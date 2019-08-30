d5632a42db8a5c11b6fa30241d9693738a29070a
16saadc <16saadc@gmail.com>
2019-08-30 18:35:37 -0400

    register screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -25,43 +25,147 @@ export default class RegistrationScreen extends Component {
 
   render () {
     return (
-      <View style={{ padding: 20 }}>
-        <Text style={{ fontSize: 27 }}>Create An Account</Text>
-        <TextInput placeholder="First Name" />
-        <TextInput placeholder="Last Name" />
-        <TextInput placeholder="Username" />
-        <TextInput placeholder="Password" />
-        <TextInput placeholder="Confirm Password" />
-        <View style={{ margin: 7 }} />
-        <Button onPress={null} title="Register" />
-        <CheckBox
-            style={{flex: 1, padding: 10}}
-            onClick={()=>{
-              this.setState({
-                  isChecked:!this.state.isChecked
-              })
-            }}
-            isChecked={this.state.isChecked}
-            rightText={"test"}
-        />
-        <Text
-          style={{ color: "blue" }}
-          onPress={this._navigateToRegister}
-          Register
-        />
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
+
+          <View style={styles.welcomeContainer}>
+            <Text style={{ fontSize: 27 }}>Create An Account</Text>
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            <TextInput
+              style={{height: 40, width: 325, marginTop: 10, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({text})}
+              value={"First Name"}
+              />
+            <TextInput
+              style={{height: 40, width: 325, marginTop: 10, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({text})}
+              value={"Last Name"}
+              />
+           <TextInput
+             style={{height: 40, width: 325, marginTop: 10, marginBottom: 10, borderColor: 'gray', borderWidth: 1}}
+             onChangeText={(text) => this.setState({text})}
+             value={"Username"}
+             />
+           <TextInput
+             style={{height: 40, width: 325, borderColor: 'gray', borderWidth: 1}}
+             onChangeText={(text) => this.setState({text})}
+             value={"Password"}
+             />
+           <TextInput
+             style={{height: 40, width: 325, marginTop: 10, marginBottom: 10, borderColor: 'gray', borderWidth: 1}}
+             onChangeText={(text) => this.setState({text})}
+             value={"Confirm Password"}
+             />
+            <Text>By registering you are agreeing to our Terms and Conditions</Text>
+          </View>
+
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+              <Text style={styles.registerButton}>
+                Create Account
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
 
-// RegistrationScreen.navigationOptions = {
-//   title: 'Registration',
-// };
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 500,
+    height: 215,
+    marginTop: 6,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  registerButton: {
+    fontSize: 14,
+    backgroundColor: '#1E960F',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
 });
