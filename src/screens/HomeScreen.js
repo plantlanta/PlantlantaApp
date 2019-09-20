62ec07abcda87d4c0deb408ac0b4662a0546c21d
derek <dweiler6@gatech.edu>
2019-09-20 03:27:36 -0400

    Updated schema, added EventListScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,10 +3,26 @@ import { StyleSheet, View, Text, Button } from 'react-native';
 import * as mutations from '../graphql/mutations';
 import { API, graphqlOperation } from 'aws-amplify';
 
+//TODO delete by end of sprint
+
+const today = new Date();
+today.setDate(today.getDate() + 57);
+const newDay = new Date();
+newDay.setDate(newDay.getDate() + 59);
 const event1 = {
-  eventId: 'qwehqwficqwi',
-  eventName: 'Event1',
-  eventDescription: 'sample event',
+  name: 'event name 3',
+  description: 'a description of the event',
+  address: '123 fake street-fake city-fake state-fakezip',
+  organization: 'org3',
+  coordinator: 'coord3',
+  coordinatorPhone: '404-402-9410',
+  coordinatorEmail: 'fake3@notreal.com',
+  rewardPointValue: 150,
+  minVolunteers: 3,
+  maxVolunteers: 10,
+  volunteers: ['vol13', 'vol23', 'vol33', 'vol43', 'vol53'],
+  startDate: today,
+  endDate: newDay,
 };
 
 createEvent = async () => {
@@ -22,7 +38,7 @@ export default HomeScreen = eventId => {
       <Button
         title="Click ME to create"
         onPress={() => {
-          createEvent();
+          // createEvent();
         }}
       />
     </View>
