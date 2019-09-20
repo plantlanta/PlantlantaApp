2ca35ea5080b5646d80e5dbf0dfc219ba46edcda
derek <dweiler6@gatech.edu>
2019-09-20 12:09:41 -0400

    Added files

---
+++ src/graphql/mutations.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createEvent = `mutation CreateEvent($input: CreateEventInput!) {
+  createEvent(input: $input) {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
+export const updateEvent = `mutation UpdateEvent($input: UpdateEventInput!) {
+  updateEvent(input: $input) {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
+export const deleteEvent = `mutation DeleteEvent($input: DeleteEventInput!) {
+  deleteEvent(input: $input) {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateEvent = `subscription OnCreateEvent {
+  onCreateEvent {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
+export const onUpdateEvent = `subscription OnUpdateEvent {
+  onUpdateEvent {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
+export const onDeleteEvent = `subscription OnDeleteEvent {
+  onDeleteEvent {
+    id
+    name
+    description
+    address
+    organization
+    coordinator
+    coordinatorPhone
+    coordinatorEmail
+    rewardPointValue
+    minVolunteers
+    maxVolunteers
+    volunteers
+    startDate
+    endDate
+  }
+}
+`;
