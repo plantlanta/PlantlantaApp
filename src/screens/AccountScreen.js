2f2d6adf9d9ed6bc661cb7724b2740f813b61683
16saadc <16saadc@gmail.com>
2019-11-13 14:59:18 -0500

    event and rewards added to account page

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,14 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { Auth, API, graphqlOperation } from 'aws-amplify';
 import {Container, Card, Body, Content, CardItem} from 'native-base';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, FlatList} from 'react-native';
 import * as queries from '../graphql/queries';
+
+
+
+const RewardItem = ({
+  id,
+  name,
+  link,
+  coupon
+}) => {
+  return (
+    <Card transparent style = {{padding: 20}}>
+    <CardItem>
+        <Text>Reward: {name}</Text>
+    </CardItem>
+    <CardItem>
+        <Text>Link: {link}</Text>
+    </CardItem>
+    <CardItem>
+        <Text>Coupon Code: {coupon}</Text>
+    </CardItem>
+    </Card>
+  );
+};
+
+const EventItem = ({
+  id,
+  timeIn,
+  timeOut
+}) => {
+  return (
+    <Card transparent style = {{padding: 20}}>
+    <CardItem>
+        <Text>Event: {id}</Text>
+    </CardItem>
+    <CardItem>
+        <Text>Check In: {timeIn}</Text>
+    </CardItem>
+    <CardItem>
+        <Text>Check Out: {timeOut}</Text>
+    </CardItem>
+    </Card>
+  );
+};
+
 
 const AccountScreen = () => {
   const [account, setAccount] = useState(
-  Auth.currentAuthenticatedUser()
-  .then(user => API.graphql(graphqlOperation(queries.getUser, {id : user.username})))
-  .then(res => {setAccount(res.data.getUser)})
+    Auth.currentAuthenticatedUser()
+      .then(user => API.graphql(graphqlOperation(queries.getUser, {id : user.username})))
+      .then(res => {setAccount(res.data.getUser)})
   );
   
   return account ? (
@@ -31,10 +75,57 @@ const AccountScreen = () => {
               <Text>Account Type:     {account.accountType}</Text>
           </CardItem>
           <CardItem>
-            <Body>
-              <Text>Event History:      {account.rewardHistory}</Text>
-              <Text>Reward History:     {account.eventHistory}</Text>
-            </Body>
+            <Text>Event History</Text>
+          </CardItem>
+          <CardItem>
+            <FlatList
+              style={{ flex: 1 }}
+              data={account.eventHistory}
+              renderItem={({ item }) => (
+                <EventItem
+                  id={item.id}
+                  timeIn={item.timeIn}
+                  timeOut={item.timeOut}
+                />
+              )}
+              ItemSeparatorComponent={() => (
+                <View
+                  style={{
+                    height: 1,
+                    width: '100%',
+                    backgroundColor: '#CED0CE'
+                  }}
+                />
+              )}
+              keyExtractor={item => item.id}
+            />
+          </CardItem>
+          <CardItem>
+            <Text>Rewards</Text>
+          </CardItem>
+          <CardItem>
+            <FlatList
+              style={{ flex: 1 }}
+              data={account.rewardHistory}
+              renderItem={({ item }) => (
+                <RewardItem
+                  id={item.id}
+                  name={item.name}
+                  link={item.link}
+                  coupon={item.coupon}
+                />
+              )}
+              ItemSeparatorComponent={() => (
+                <View
+                  style={{
+                    height: 1,
+                    width: '100%',
+                    backgroundColor: '#CED0CE'
+                  }}
+                />
+              )}
+              keyExtractor={item => item.id}
+            />
           </CardItem>
           </Card>
         </Content>
